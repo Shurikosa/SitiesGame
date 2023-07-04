c65741a934a85f6ffca08245fd6a7be5cb459d5f
Dimkageo <dimkageo1@gmail.com>
2023-07-04 20:12:55 +0300

    Добавлене вітальні вікна

--- src/main/java/Fremes/Main.java
+++ src/main/java/Fremes/Main.java
@@ -3,6 +3,7 @@ package Fremes;
 public class Main {
     public static void main(String[] args) {
 
-        new WelcomeFrame();
+//        new WelcomeFrame();
+        System.out.println("hello");
     }
 }
